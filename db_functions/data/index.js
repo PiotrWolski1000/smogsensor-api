d8baab5b07f47aa5ac9d54ed0efc0012527b4d51
piotr Wolski <p.wolski1000@gmail.com>
2019-06-07 12:17:24 +0200

    creates files for specific db tables

---
+++ db_functions/data/index.js
@@ -0,0 +1,3 @@
+const db = require('./../../db/pgpool')
+var pool = db.getPool()
+
--- db_functions/sensors/index.js
+++ db_functions/sensors/index.js
@@ -2,13 +2,7 @@ const db = require('./../../db/pgpool')
 var pool = db.getPool()
 
 function insertSensor(settings, callback) {
-    // pool.query('SELECT * FROM stations;', function (err, res) {
-    //     if (err) {
-    //         callback(err, null)
-    //     } else {
-    //         callback(null, res.rows[0])
-    //     }
-    // });
+    // console.log('inserting these values to sensors table: ', settings);
     pool.query('INSERT INTO sensors(id, id_station, parameter_name, formula_name) VALUES($1, $2, $3, $4) RETURNING *;', settings, function (err, res) {
         if (err) {
             callback(err, null)
