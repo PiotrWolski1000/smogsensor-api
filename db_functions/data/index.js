2cb7eacab619859e768ef3aca77743df8a1bec95
piotr Wolski <p.wolski1000@gmail.com>
2019-06-10 17:05:27 +0200

    api get call move to another file (from index.js)

--- db_functions/data/index.js
+++ db_functions/data/index.js
@@ -1,3 +1,15 @@
 const db = require('./../../db/pgpool')
 var pool = db.getPool()
 
+function insertData(settings, callback) {
+    // console.log('inserting these values to sensors table: ', settings);
+    pool.query('INSERT INTO data(id, date, value) VALUES($1, $2, $3) RETURNING *;', settings, function (err, res) {
+        if (err) {
+            callback(err, null)
+        } else {
+            callback(null, res.rows[0])
+        }
+    });
+}
+
+exports.insertData = insertData
